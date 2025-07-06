7bc861894f9a6fc5742cbf00dd31d1ee305adbfd
NadaRamadan <nada7.ra@gmail.com>
2025-07-06 21:36:30 +0300

    added Events proect event args class

---
+++ Events/TemperatureChangedEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    public class TemperatureChangedEventArgs : EventArgs
+    {
+        public double Temperature { get; }
+
+        public string Location { get; }
+
+        public TemperatureChangedEventArgs(double Temperature, string Location)
+        {
+            this.Temperature = Temperature; 
+            this.Location = Location;
+        }
+    }
+}
